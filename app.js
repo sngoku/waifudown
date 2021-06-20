01942924b2f722cc4463d990d8803ae232fabc36
snGoku <sngoku.2021.com>
2021-06-20 19:30:34 +0430

    second commit

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const fs = require('fs');
 const Path = require('path');
 const Axios = require('axios');
 const randomize = require('randomatic');
-const { throws } = require('assert');
 
 async function downloadImage(type = 'nsfw', category = 'waifu', path = Path.resolve(__dirname, 'images')) {
     defaultPath = Path.join(path, type, category);
 
-    const url = 'https://api.waifu.pics/' + type + '/' + category;
+    const url = 'https://waifu.vercel.app/' + type + '/' + category;
 
 
     if (!fs.existsSync(defaultPath)) {
@@ -26,38 +25,32 @@ async function downloadImage(type = 'nsfw', category = 'waifu', path = Path.reso
     })
 
     try {
-        if (img.status === 200) {
 
+        const writer = fs.createWriteStream(imgPath);
 
-            const writer = fs.createWriteStream(imgPath);
+        img.data.pipe(writer);
 
-            img.data.pipe(writer);
+        return new Promise((resolve, reject) => {
 
-            return new Promise((resolve, reject) => {
+            writer.on('finish', resolve);
 
-                writer.on('finish', resolve);
+            writer.on('error', reject);
 
-                writer.on('error', reject);
-
-            });
-
-        } else {
-            downloadImage();
-        }
-
+        }).catch(err => 
+            {console.log(err);
+        });
 
     } catch (error) {
+
         console.log(error);
+
     }
 
 }
 
-
-
 function downloadManyImages(numberOfImage = 1, type, category, path) {
     for (let i = 0; i < numberOfImage; i++) {
         downloadImage(type, category, path);
-        console.log('success');
     }
 
 }
@@ -65,6 +58,6 @@ function downloadManyImages(numberOfImage = 1, type, category, path) {
 //Enter as many images as you want followed by a ","  then type and then the category and finally the path (all optional 
 //but if you want to pass any options you should include the options that came before)
 //Example:
-// downloadManyImages(3,'nsfw','waifu','c:/Users/SMAM/Desktop');
+// downloadManyImages(3,'nsfw','waifu','c:/Users/administrator/Desktop');
 
-downloadManyImages(10, 'nsfw', 'waifu', 'C:/Users/SMAM/Desktop');
+downloadManyImages(5);
